98cae69a71eb88a195dcb83da7b49dbcd4211c71
dolphinalt <dolphins.supergeinus034@gmail.com>
2023-02-15 14:52:37 -0800

    sophia

--- assets/js/tasks/calculateSleepTime.js
+++ assets/js/tasks/calculateSleepTime.js
@@ -1,14 +1,70 @@
-// 1 means completed, 0 means not
-function httpGet() {
+// // 1 means completed, 0 means not
+// class http {
+//     GET () {
+//         var xmlHttp = new XMLHttpRequest();
+//         xmlHttp.open("GET", "http://seal.nighthawkcodingsociety.com/api/users/", false); // false for synchronous request
+//         xmlHttp.send(null);
+//         json = JSON.parse(xmlHttp.responseText);
+//         console.log(json);
+//         return json;
+//     }
+//     POST () {
+//         var xmlHttp = new XMLHttpRequest();
+//         xmlHttp.open("POST", "http://seal.nighthawkcodingsociety.com/api/users/", false); // false for synchronous request
+//         xmlHttp.send(null);
+//         json = JSON.parse(xmlHttp.responseText);
+//         return json;
+//     }
+//     DELETE () {
+//         var xmlHttp = new XMLHttpRequest();
+//         xmlHttp.open("DELETE", "http://seal.nighthawkcodingsociety.com/api/users/", false); // false for synchronous request
+//         xmlHttp.send(null);
+//         json = JSON.parse(xmlHttp.responseText);
+//         return json;
+//     }
+//     PUT () {
+//         var xmlHttp = new XMLHttpRequest();
+//         xmlHttp.open("PUT", "http://seal.nighthawkcodingsociety.com/api/users/", false); // false for synchronous request
+//         xmlHttp.send(null);
+//         json = JSON.parse(xmlHttp.responseText);
+//         return json;
+//     }
+// }
+
+function GET () {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", "http://seal.nighthawkcodingsociety.com/api/users/", false); // false for synchronous request
+    xmlHttp.send(null);
+    json = JSON.parse(xmlHttp.responseText);
+    console.log(json);
+    return json;
+}
+function POST () {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("POST", "https://seal.nighthawkcodingsociety.com/api/users/time", false); // false for synchronous request
+    xmlHttp.send(null);
+    json = JSON.parse(xmlHttp.responseText);
+    return json;
+}
+function DELETE () {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("DELETE", "http://seal.nighthawkcodingsociety.com/api/users/", false); // false for synchronous request
+    xmlHttp.send(null);
+    json = JSON.parse(xmlHttp.responseText);
+    return json;
+}
+function PUT () {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("PUT", "http://seal.nighthawkcodingsociety.com/api/users/", false); // false for synchronous request
     xmlHttp.send(null);
     json = JSON.parse(xmlHttp.responseText);
     return json;
 }
 
+// get = GET();
 
-get = httpGet();
+
+
 getTasks = get[0]["tasks"];
 getTimes = get[0]["times"];
 getTasksCompleted = get[0]["tasksCompleted"];
